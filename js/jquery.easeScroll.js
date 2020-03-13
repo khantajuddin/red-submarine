19be1a0fe4ec57dcf2e00ce1cf49b00f18bf3eed
Tajuddin Khan <khantaj65@gmail.com>
2020-03-13 15:08:42 +0530

    Update jquery.easeScroll.js

--- js/jquery.easeScroll.js
+++ js/jquery.easeScroll.js
@@ -1,256 +1,153 @@
-$.fn.easeScroll = function(options) {
-    ! function() {
-        function e() {
-            var e = !1;
-            e && c("keydown", r), v.keyboardSupport && !e && u("keydown", r)
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * User: phil
+ * Date: 15/11/12
+ * Time: 11:04 AM
+ *
+ */
+(function ($) {
+
+    var self = this, container, running=false, currentY = 0, targetY = 0, oldY = 0, maxScrollTop= 0, minScrollTop, direction, onRenderCallback=null,
+            fricton = 0.95, // higher value for slower deceleration
+            vy = 0,
+            stepAmt = 1,
+            minMovement= 0.1,
+            ts=0.1;
+
+    var updateScrollTarget = function (amt) {
+        targetY += amt;
+        vy += (targetY - oldY) * stepAmt;
+      
+        oldY = targetY;
+
+
+    }
+    var render = function () {
+        if (vy < -(minMovement) || vy > minMovement) {
+
+            currentY = (currentY + vy);
+            if (currentY > maxScrollTop) {
+                currentY = vy = 0;
+            } else if (currentY < minScrollTop) {
+                    vy = 0;
+                    currentY = minScrollTop;
+                }
+           
+            container.scrollTop(-currentY);
+
+            vy *= fricton;
+            
+         //   vy += ts * (currentY-targetY);
+            // scrollTopTweened += settings.tweenSpeed * (scrollTop - scrollTopTweened);
+            // currentY += ts * (targetY - currentY);
+
+            if(onRenderCallback){
+                onRenderCallback();
+            }
+        }
+    }
+    var animateLoop = function () {
+        if(! running)return;
+        requestAnimFrame(animateLoop);
+        render();
+        //log("45","animateLoop","animateLoop", "",stop);
+    }
+    var onWheel = function (e) {
+        e.preventDefault();
+        var evt = e.originalEvent;
+       
+        var delta = evt.detail ? evt.detail * -1 : evt.wheelDelta / 40;
+        var dir = delta < 0 ? -1 : 1;
+        if (dir != direction) {
+            vy = 0;
+            direction = dir;
         }
 
-        function t() {
-            if (document.body) {
-                var t = document.body,
-                    o = document.documentElement,
-                    n = window.innerHeight,
-                    r = t.scrollHeight;
-                if (S = document.compatMode.indexOf("CSS") >= 0 ? o : t, w = t, e(), x = !0, top != self) y = !0;
-                else if (r > n && (t.offsetHeight <= n || o.offsetHeight <= n)) {
-                    var a = !1,
-                        i = function() {
-                            a || o.scrollHeight == document.height || (a = !0, setTimeout(function() {
-                                o.style.height = document.height + "px", a = !1
-                            }, 100))
-                        };
-                    if (o.style.height = "auto", setTimeout(i, 10), S.offsetHeight <= n) {
-                        var l = document.createElement("div");
-                        l.style.clear = "both", t.appendChild(l)
+        //reset currentY in case non-wheel scroll has occurred (scrollbar drag, etc.)
+        currentY = -container.scrollTop();
+        
+        updateScrollTarget(delta);
+    }
+
+    /*
+     * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+     */
+    window.requestAnimFrame = (function () {
+        return  window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function (callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                }; 
+              
+                
+    })();
+
+    /*
+     * http://jsbin.com/iqafek/2/edit
+     */
+    var normalizeWheelDelta = function () {
+        // Keep a distribution of observed values, and scale by the
+        // 33rd percentile.
+        var distribution = [], done = null, scale = 30;
+        return function (n) {
+            // Zeroes don't count.
+            if (n == 0) return n;
+            // After 500 samples, we stop sampling and keep current factor.
+            if (done != null) return n * done;
+            var abs = Math.abs(n);
+            // Insert value (sorted in ascending order).
+            outer: do { // Just used for break goto
+                for (var i = 0; i < distribution.length; ++i) {
+                    if (abs <= distribution[i]) {
+                        distribution.splice(i, 0, abs);
+                        break outer;
                     }
                 }
-                v.fixedBackground || b || (t.style.backgroundAttachment = "scroll", o.style.backgroundAttachment = "scroll")
-            }
-        }
-
-        function o(e, t, o, n) {
-            if (n || (n = 1e3), d(t, o), 1 != v.accelerationMax) {
-                var r = +new Date,
-                    a = r - C;
-                if (a < v.accelerationDelta) {
-                    var i = (1 + 30 / a) / 2;
-                    i > 1 && (i = Math.min(i, v.accelerationMax), t *= i, o *= i)
-                }
-                C = +new Date
-            }
-            if (M.push({
-                x: t,
-                y: o,
-                lastX: 0 > t ? .99 : -.99,
-                lastY: 0 > o ? .99 : -.99,
-                start: +new Date
-            }), !T) {
-                var l = e === document.body,
-                    u = function() {
-                        for (var r = +new Date, a = 0, i = 0, c = 0; c < M.length; c++) {
-                            var s = M[c],
-                                d = r - s.start,
-                                f = d >= v.animationTime,
-                                h = f ? 1 : d / v.animationTime;
-                            v.pulseAlgorithm && (h = p(h));
-                            var m = s.x * h - s.lastX >> 0,
-                                w = s.y * h - s.lastY >> 0;
-                            a += m, i += w, s.lastX += m, s.lastY += w, f && (M.splice(c, 1), c--)
-                        }
-                        l ? window.scrollBy(a, i) : (a && (e.scrollLeft += a), i && (e.scrollTop += i)), t || o || (M = []), M.length ? E(u, e, n / v.frameRate + 1) : T = !1
-                    };
-                E(u, e, 0), T = !0
-            }
-        }
-
-        function n(e) {
-            x || t();
-            var n = e.target,
-                r = l(n);
-            if (!r || e.defaultPrevented || s(w, "embed") || s(n, "embed") && /\.pdf/i.test(n.src)) return !0;
-            var a = e.wheelDeltaX || 0,
-                i = e.wheelDeltaY || 0;
-            return a || i || (i = e.wheelDelta || 0), !v.touchpadSupport && f(i) ? !0 : (Math.abs(a) > 1.2 && (a *= v.stepSize / 120), Math.abs(i) > 1.2 && (i *= v.stepSize / 120), o(r, -a, -i), void e.preventDefault())
-        }
-
-        function r(e) {
-            var t = e.target,
-                n = e.ctrlKey || e.altKey || e.metaKey || e.shiftKey && e.keyCode !== H.spacebar;
-            if (/input|textarea|select|embed/i.test(t.nodeName) || t.isContentEditable || e.defaultPrevented || n) return !0;
-            if (s(t, "button") && e.keyCode === H.spacebar) return !0;
-            var r, a = 0,
-                i = 0,
-                u = l(w),
-                c = u.clientHeight;
-            switch (u == document.body && (c = window.innerHeight), e.keyCode) {
-                case H.up:
-                    i = -v.arrowScroll;
-                    break;
-                case H.down:
-                    i = v.arrowScroll;
-                    break;
-                case H.spacebar:
-                    r = e.shiftKey ? 1 : -1, i = -r * c * .9;
-                    break;
-                case H.pageup:
-                    i = .9 * -c;
-                    break;
-                case H.pagedown:
-                    i = .9 * c;
-                    break;
-                case H.home:
-                    i = -u.scrollTop;
-                    break;
-                case H.end:
-                    var d = u.scrollHeight - u.scrollTop - c;
-                    i = d > 0 ? d + 10 : 0;
-                    break;
-                case H.left:
-                    a = -v.arrowScroll;
-                    break;
-                case H.right:
-                    a = v.arrowScroll;
-                    break;
-                default:
-                    return !0
-            }
-            o(u, a, i), e.preventDefault()
-        }
-
-        function a(e) {
-            w = e.target
-        }
-
-        function i(e, t) {
-            for (var o = e.length; o--;) z[N(e[o])] = t;
-            return t
-        }
-
-        function l(e) {
-            var t = [],
-                o = S.scrollHeight;
-            do {
-                var n = z[N(e)];
-                if (n) return i(t, n);
-                if (t.push(e), o === e.scrollHeight) {
-                    if (!y || S.clientHeight + 10 < o) return i(t, document.body)
-                } else if (e.clientHeight + 10 < e.scrollHeight && (overflow = getComputedStyle(e, "").getPropertyValue("overflow-y"), "scroll" === overflow || "auto" === overflow)) return i(t, e)
-            } while (e = e.parentNode)
-        }
-
-        function u(e, t, o) {
-            window.addEventListener(e, t, o || !1)
-        }
-
-        function c(e, t, o) {
-            window.removeEventListener(e, t, o || !1)
-        }
-
-        function s(e, t) {
-            return (e.nodeName || "").toLowerCase() === t.toLowerCase()
-        }
-
-        function d(e, t) {
-            e = e > 0 ? 1 : -1, t = t > 0 ? 1 : -1, (k.x !== e || k.y !== t) && (k.x = e, k.y = t, M = [], C = 0)
-        }
-
-        function f(e) {
-            if (e) {
-                e = Math.abs(e), D.push(e), D.shift(), clearTimeout(A);
-                var t = D[0] == D[1] && D[1] == D[2],
-                    o = h(D[0], 120) && h(D[1], 120) && h(D[2], 120);
-                return !(t || o)
-            }
-        }
-
-        function h(e, t) {
-            return Math.floor(e / t) == e / t
-        }
-
-        function m(e) {
-            var t, o, n;
-            return e *= v.pulseScale, 1 > e ? t = e - (1 - Math.exp(-e)) : (o = Math.exp(-1), e -= 1, n = 1 - Math.exp(-e), t = o + n * (1 - o)), t * v.pulseNormalize
-        }
-
-        function p(e) {
-            return e >= 1 ? 1 : 0 >= e ? 0 : (1 == v.pulseNormalize && (v.pulseNormalize /= m(1)), m(e))
-        }
-
-        var settings = $.extend({
-            // These are the defaults.
-            frameRate: 60,
-            animationTime: 1000,
-            stepSize: 120,
-            pulseAlgorithm: !0,
-            pulseScale: 8,
-            pulseNormalize: 1,
-            accelerationDelta: 20,
-            accelerationMax: 1,
-            keyboardSupport: !0,
-            arrowScroll: 50,
-            touchpadSupport: !0,
-            fixedBackground: !0
-        }, options );
-
-        var w, g = {
-            frameRate: settings.frameRate,
-            animationTime: settings.animationTime,
-            stepSize: settings.stepSize,
-            pulseAlgorithm: settings.pulseAlgorithm,
-            pulseScale: settings.pulseScale,
-            pulseNormalize: settings.pulseNormalize,
-            accelerationDelta: settings.accelerationDelta,
-            accelerationMax: settings.accelerationMax,
-            keyboardSupport: settings.keyboardSupport,
-            arrowScroll: settings.arrowScroll,
-            touchpadSupport: settings.touchpadSupport,
-            fixedBackground: settings.fixedBackground,
-            excluded: ""
-        },
-            v = g,
-            b = !1,
-            y = !1,
-            k = {
-                x: 0,
-                y: 0
-            },
-            x = !1,
-            S = document.documentElement,
-            D = [120, 120, 120],
-            H = {
-                left: 37,
-                up: 38,
-                right: 39,
-                down: 40,
-                spacebar: 32,
-                pageup: 33,
-                pagedown: 34,
-                end: 35,
-                home: 36
-            },
-            v = g,
-            M = [],
-            T = !1,
-            C = +new Date,
-            z = {};
-        setInterval(function() {
-            z = {}
-        }, 1e4);
-        var A, N = function() {
-                var e = 0;
-                return function(t) {
-                    return t.uniqueID || (t.uniqueID = e++)
-                }
-            }(),
-            E = function() {
-                return window.requestAnimationFrame || window.webkitRequestAnimationFrame || function(e, t, o) {
-                    window.setTimeout(e, o || 1e3 / 60)
-                }
-            }(),
-            K = /chrome|iPad/i.test(window.navigator.userAgent),
-            L = "onmousewheel" in document;
-        L && K && (u("mousedown", a), u("mousewheel", n), u("load", t))
+                distribution.push(abs);
+            } while (false);
+            // Factor is scale divided by 33rd percentile.
+            var factor = scale / distribution[Math.floor(distribution.length / 3)];
+            if (distribution.length == 500) done = factor;
+            return n * factor;
+        };
     }();
-}
 
 
+    $.fn.smoothWheel = function () {
+        //  var args = [].splice.call(arguments, 0);
+        var options = jQuery.extend({}, arguments[0]);
+        return this.each(function (index, elm) {
+
+            if(!('ontouchstart' in window)){
+                container = $(this);
+                container.bind("mousewheel", onWheel);
+                container.bind("DOMMouseScroll", onWheel);
+
+                //set target/old/current Y to match current scroll position to prevent jump to top of container
+                targetY = oldY = container.get(0).scrollTop;
+                currentY = -targetY;
+                
+                minScrollTop = container.get(0).clientHeight - container.get(0).scrollHeight;
+                if(options.onRender){
+                    onRenderCallback = options.onRender;
+                }
+                if(options.remove){
+                    log("122","smoothWheel","remove", "");
+                    running=false;
+                    container.unbind("mousewheel", onWheel);
+                    container.unbind("DOMMouseScroll", onWheel);
+                }else if(!running){
+                    running=true;
+                    animateLoop();
+                }
+
+            }
+        });
+    };
+
+
+})(jQuery);
